c4e03f9ac859a9dcb8602685f31718a89faf2f09
Enoch Chukwudi <enochc.official@gmail.com>
2026-06-18 11:37:49 +0100

    Add Google Analytics 4 (G-G767GZ6PR1) with page view tracking on every route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
@@ -6,8 +7,15 @@ import Collections from "./pages/Collections";
 import Faq from "./pages/Faq";
 import Checkout from "./components/Checkout";
 import Dynamic from "./components/Dynamic";
+import ReactGA from "react-ga4";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+  }, [location]);
+
   return (
     <>
       <Navbar />
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from "./App.jsx";
 import ScrollToTop from "./components/ScrollToTop";
 import WhatsAppFloat from "./components/WhatsAppFloat";
 import { CartProvider } from "./context/CartContext";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-G767GZ6PR1");
 
 createRoot(document.getElementById("root")).render(
   <Router>
